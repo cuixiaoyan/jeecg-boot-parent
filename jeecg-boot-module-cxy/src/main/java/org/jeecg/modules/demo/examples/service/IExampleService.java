a6cba7b452c2707211a4f49150e9426c91b45edf
cuixiaoyan <56696508@qq.com>
2019-09-12 15:04:13 +0800

    开发和生产环境的配置未提交 个人使用

---
+++ jeecg-boot-module-cxy/src/main/java/org/jeecg/modules/demo/examples/service/IExampleService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.demo.examples.service;
+
+import org.jeecg.modules.demo.examples.entity.Example;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 例子
+ * @Author: jeecg-boot
+ * @Date:   2019-09-07
+ * @Version: V1.0
+ */
+public interface IExampleService extends IService<Example> {
+
+}
